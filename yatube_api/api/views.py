d591762b8e95e4bdcb13b7b0808a2607384b1b51
Максим <kasyak999@yandex.ru>
2024-09-01 14:12:14 +0300

    up

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,37 +2,23 @@ from posts.models import Post, Group, Comment
 from .serializers import (
     PostSerializer, GroupSerializer, CommentSerializer)
 from rest_framework import viewsets
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, BasePermission
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    """Проверяет, является ли пользователь автором объекта."""
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user or request.method == 'GET'
 
 
 class PostViewSet(viewsets.ModelViewSet):
     """получаем список всех постов или создаём новый пост."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def update(self, request, partial=True, pk=None):
-        post = self.get_object()
-        if post.author == self.request.user:
-            return super().update(request, pk)
-        return Response(status=status.HTTP_403_FORBIDDEN)
-
-    def partial_update(self, request, partial=True, pk=None):
-        post = self.get_object()
-        if post.author == self.request.user:
-            return super().partial_update(request, pk)
-        return Response(status=status.HTTP_403_FORBIDDEN)
-
-    def destroy(self, request, pk):
-        post = self.get_object()
-        if post.author == self.request.user:
-            return super().destroy(request, pk)
-        return Response(status=status.HTTP_403_FORBIDDEN)
 
 
 class GroupsViewSet(viewsets.ReadOnlyModelViewSet):
@@ -44,6 +30,7 @@ class GroupsViewSet(viewsets.ReadOnlyModelViewSet):
 class PostsCommentViewSet(viewsets.ModelViewSet):
     """получаем список всех комментариев."""
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         return Comment.objects.filter(post=self.kwargs['post_pk'])
@@ -52,21 +39,3 @@ class PostsCommentViewSet(viewsets.ModelViewSet):
         serializer.save(
             author=self.request.user,
             post=Post.objects.get(pk=self.kwargs['post_pk']))
-
-    def update(self, request, post_pk, pk=None):
-        comment = self.get_object()
-        if comment.author == self.request.user:
-            return super().update(request, pk)
-        return Response(status=status.HTTP_403_FORBIDDEN)
-
-    def partial_update(self, request, post_pk, pk=None):
-        comment = self.get_object()
-        if comment.author == self.request.user:
-            return super().partial_update(request, pk)
-        return Response(status=status.HTTP_403_FORBIDDEN)
-
-    def destroy(self, request, post_pk, pk):
-        comment = self.get_object()
-        if comment.author == self.request.user:
-            return super().destroy(request, pk)
-        return Response(status=status.HTTP_403_FORBIDDEN)
